81e720cc3721907259176fd99083880dbc1dafcd
evanandrewson <evanandrewson@gmail.com>
2019-07-11 20:51:14 -0700

    finished functionality

---
+++ app.js
@@ -0,0 +1,15 @@
+import isEven from './is-even.js';
+const numberInput = document.getElementById('number')
+
+button.onclick = function() {
+    const number = Number(numberInput.value);
+    const result = isEven(number);
+    console.log(result)
+    if(result === true) {
+        even.classList.remove('hidden')
+        odd.classList.add('hidden')
+    } else {
+        even.classList.add('hidden')
+        odd.classList.remove('hidden')
+    }
+}
